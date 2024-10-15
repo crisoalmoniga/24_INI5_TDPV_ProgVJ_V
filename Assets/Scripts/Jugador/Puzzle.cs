0c999f94e90ab054fb4422f3ea446a1a3445e90a
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-10-14 22:09:10 -0300

    Listos coleccionables
    El tag tenia tres "c" al escribirlo

--- Assets/Scripts/Jugador/Puzzle.cs
+++ Assets/Scripts/Jugador/Puzzle.cs
@@ -37,7 +37,7 @@ public class Puzzle : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.gameObject.CompareTag("Colecccionable")) { return; }
+        if (!collision.gameObject.CompareTag("Coleccionable")) { return; }
 
         GameObject nuevoColeccionable = collision.gameObject;
         nuevoColeccionable.SetActive(false);
