3d8513360149bf5ee507bd7410b90e8a8141004c
Criso <68233846+crisoalmoniga@users.noreply.github.com>
2024-10-20 18:26:17 -0300

    Fix Boss Final

--- Assets/Jefefinal.cs
+++ Assets/Jefefinal.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JefeFinal : MonoBehaviour
+public class Jefefinal : MonoBehaviour
 {
     [SerializeField] float tiempoEntreDisparos;
     [SerializeField] float tiempoEntreEmbestidas;
